a0448dd7cc7c415d620d2a69d0a522eff4fb3609
Parth Sinha <sinhaparth555@gmail.com>
2025-04-16 19:22:17 +0100

    added project

---
+++ src/main/java/com/jobsync/jobsync/JobsyncApplication.java
@@ -0,0 +1,13 @@
+package com.jobsync.jobsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobsync/jobsync/JobsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobsync.jobsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
